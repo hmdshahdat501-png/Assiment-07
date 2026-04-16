856f1a8c666008d5da387b44c9682f582bb4d0f9
hmdshahdat501-png <hmdshahdat501@gmail.comm>
2026-04-16 16:19:28 +0600

    re deploy

--- src/Component/Timeline/Timeline.jsx
+++ src/Component/Timeline/Timeline.jsx
@@ -7,9 +7,37 @@ import { IoIosMailOpen } from "react-icons/io";
 const Timeline = () => {
   const {call, setCall} = useContext(MyContext);
   const now = new Date();
+  const calldata = call.filter(dat => dat.action === "call");
+  const massgdata = call.filter(dat => dat.action === "massage");
+  const veidodata = call.filter(dat => dat.action === "vedio");
+  
     return (
+
         <div className="mx-auto  container">
+            
+        {/* <div className="mt-4 text-center">
+        <select defaultValue="Pick a color" className="select">
+  <option onClick={callhandel}>Call</option>
+  <option onClick={massahandl}>Massage</option>
+  <option onClick={veidodahandl}>Vdioe</option>
+</select>
+        </div> */}
        <h2 className="text-4xl font-bold mt-8">Timeline </h2>
+       <select
+  onChange={(e) => {
+    const value = e.target.value;
+
+    if (value === "call") setCall(calldata);
+    else if (value === "message") setCall(massgdata);
+    else if (value === "video") setCall(veidodata);
+    else setCall(call);
+  }}
+  className="select mt-4">
+  <option value="all">All</option>
+  <option value="call">Call</option>
+  <option value="message">Message</option>
+  <option value="video">Video</option>
+</select>
        <div>
         {
             call.map(data => <div className="flex items-center gap-4 border-2 p-4 mt-4 rounded-xl shadow border-gray-400 mx-4 lg:mx-0">
